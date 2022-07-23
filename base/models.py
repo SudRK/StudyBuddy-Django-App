caf0fc9684d6aa03f34a84293337dd2875acfb05
Sudeep Khokhar <104437208+SudRK@users.noreply.github.com>
2022-07-23 14:18:13 +0530

    participant email style change

--- base/models.py
+++ base/models.py
@@ -1,7 +1,35 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
 
 # Create your models here.
+class CustomUserManager(BaseUserManager):
+    """
+    Custom user model manager where email is the unique identifier
+    for authentication instead of usernames.
+    """
+
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError(_('Users must have an email address'))
+        email = self.normalize_email(email)
+
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        return self.create_user(email, password, **extra_fields)
+
 
 class User(AbstractUser):
     name = models.CharField(max_length=200, null=True) 
@@ -9,9 +37,10 @@ class User(AbstractUser):
     bio = models.TextField(null=True)
     is_active = models.BooleanField(default=False)
     avatar = models.ImageField(null=True, default="avatar.svg")
+    objects = CustomUserManager()
     
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = ['username']
 
 
 class Topic(models.Model):
@@ -56,3 +85,4 @@ class ForgotPassword(models.Model):
 
     def __str__(self):
         return self.user.username
+    
